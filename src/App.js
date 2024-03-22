a36fd6914a993a1e8bc971642cf1c61dabee806c
Agafina <98034773+Agafina@users.noreply.github.com>
2024-03-22 19:15:47 +0100

    using new features of react-router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import { BrowserRouter as Router , Routes, Route, Link, NavLink} from "react-router-dom";
+import { Route,  NavLink, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import DisplayNotes from "./pages/DisplayNotes";
-import AddNote from "./pages/AddNote";
-import NoteContextProvider from "./context/noteContext";
+
+
+import PageLayout from "./layout/PageLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path ='/' element = {<PageLayout />}>
+          <Route index element ={<Home />} />
+          <Route path="about" element ={<About />} />
+    </Route>
+  )
+)
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <NoteContextProvider>
-        <nav>
-        <ul>
-          <NavLink to ="/">Home</NavLink>
-          <NavLink to = 'about'>About</NavLink>
-        </ul>
-      </nav>
       
-          <Routes>
-            <Route  path="/" element={<Home />}/>
-            <Route  path="about" element ={<About />} />
-           
-          </Routes>
-          <DisplayNotes />
-            <AddNote />
-        </NoteContextProvider>
-      </Router>
+    <RouterProvider router ={router}/>
 
     </div>
   );
